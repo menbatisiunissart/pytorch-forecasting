f2e3d74e4425edd1350b6597863b1c992d464890
menbatisiunissart <aboulenger@gmail.com>
2022-09-29 12:53:08 +0100

    added "load_best_model_path" logic added "main" logic evaluation module

--- examples/evaluation.py
+++ examples/evaluation.py
@@ -1,12 +1,14 @@
 
 from pytorch_forecasting.metrics import SMAPE
-from pytorch_lightning import LightningModule, Trainer
+from pytorch_forecasting.models import TemporalFusionTransformer
+from pytorch_forecasting.data import TimeSeriesDataSet
+from pytorch_lightning import LightningModule
 from torch.utils.data import DataLoader
 from torch import cat
 
 ## Evaluate performance
 def evaluate(
-    trainer: Trainer,
+    checkpoint_path: str,
     model: LightningModule,
     test_dataloader: DataLoader
     ):
@@ -14,9 +16,7 @@ def evaluate(
 
     # load the best model according to the validation loss
     # (given that we use early stopping, this is not necessarily the last epoch)
-    best_model_path = trainer.checkpoint_callback.best_model_path
-    best_tft = model.load_from_checkpoint(best_model_path)
-
+    best_tft = model.load_from_checkpoint(checkpoint_path)
 
     # calcualte mean absolute error on validation set
     actuals = cat([y[0] for x, y in iter(test_dataloader)])
@@ -30,9 +30,7 @@ def evaluate(
     for idx in range(10):  # plot 10 examples
         best_tft.plot_prediction(x, raw_predictions, idx=idx, add_loss_to_title=True);
 
-
     ### Worst performers
-
     # calcualte metric by which to display
     predictions = best_tft.predict(test_dataloader)
     mean_losses = SMAPE(reduction="none")(predictions, actuals).mean(1)
@@ -46,3 +44,29 @@ def evaluate(
 
     print("Press Enter to close all plots and continue ...")
     input()
+
+def load_best_model_path(
+        log_dir: str='lightning_logs'
+    ):
+    import json
+    json_path = log_dir+'/'+'best_model_path.json'
+    with open(json_path) as f:
+        d = json.load(f)
+        print(d)
+        checkpoint_path = d.get("checkpoint_path")
+    return checkpoint_path
+
+if __name__ == '__main__':
+    # load datasets
+    validation = TimeSeriesDataSet.load("validation.pkl")
+    batch_size = 64
+    num_workers = 8
+    val_dataloader = validation.to_dataloader(train=False, batch_size=batch_size, num_workers=num_workers)
+    # find_best_model_path
+    checkpoint_path = load_best_model_path()
+    # evaluate model
+    evaluate(
+        checkpoint_path=checkpoint_path,
+        model=TemporalFusionTransformer,
+        test_dataloader=val_dataloader
+        )
